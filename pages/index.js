78db81ace1d4780705b479c26f411b1cc298f7a1
Alena Burkova <alenaburkova@imaginativesoftware.co.uk>
2022-12-20 20:04:15 +0400

    greeting update

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi all! I'm Alyona and I'm tired</p>
+        <p>Hi all! This is for testing preview deployment</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
